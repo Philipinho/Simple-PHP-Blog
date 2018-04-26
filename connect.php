c6ea5aae33c57fd17ffe6fb74946dcdd5a952ec2
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-26 01:15:48 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,15 @@
-<?ph
+<?php
+$host = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "simpleblog";
+
+$dbcon = mysqli_connect("$host, $dbuser, $dbpass);
+if(!$dbcon) {
+die("Failed to connect". mysqli_connect_error());
+}
+mysqli_select_db($dbname);
+
+mysqli_close($dbcon);
 
  ?>
