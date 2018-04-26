087409d4749697d7a04991847893187822ab7084
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-26 02:35:20 +0100

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -3,14 +3,21 @@ include('header.php');
 include('connect.php');
 
 if(isset($_POST['submit'])) {
-  $id = htmlentities(
-
+  $id = sanitize($_GET['id']);
+  $title = sanitize($_POST['title']);
+  $description = sanitize($_POST['description']);
+  $date = sanitize($_POST['date']);
 
 $sql = "INSERT INTO blog (id, title, description, date) VALUES($id, $title, $description, $date)";
 
 $result = mysqli_query($dbcon, $sql);
-
-
-
+  if(!$result) {
+    echo "failed to post" . mysqli_connect_error();
+  }
+  else {
+    echo "Posted succesfully";
+  }
+  
+  mysqli_close($dbcon);
 
 ?>
