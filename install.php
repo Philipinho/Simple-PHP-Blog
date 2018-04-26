0210dd827e6273174770a48269a90b5f5b214368
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-26 02:04:17 +0100

    Update install.php

--- install.php
+++ install.php
@@ -1,1 +1,15 @@
 
+<?php
+include('connect.php');
+
+$sql = "CREATE TABLE blog ('id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, title VARCHAR(80) NOT NULL, description text NOT NULL, date TIMESTAMP')";
+$result = mysqli_query($dbcon, $sql);
+if(!$result) {
+  die("failed to create database". mysqli_connect_error());
+}
+else {
+  echo "Database created succesfully";
+}
+ mysqli_close($dbcon);
+
+?>
