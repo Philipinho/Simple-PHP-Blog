5191fe76272ae253a262ecf4e682d9a4ef36491e
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-26 02:54:24 +0100

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -2,6 +2,26 @@
 include('header.php');
 include('connect.php');
 
+// Protect page
+$user = "admin";
+$pin = "blabla";
+if(isset($_POST['access'])) {
+  if(sanitize($user) == $user && sanitize($pin) == $pin) {
+    echo "<script>alert("Access Granted")</script>";
+  }
+  else {
+    die("Access Denied");
+  }
+  if(!isset($_POST['user']) && !isset($_POST['pin'])) {
+    echo "<form action="" method="POST" class="w3-panel w3-card-4">
+    <label>User</label>
+    <input type="text" name="user" class="w3-input w3-border" required>
+    <label>Pin</label>
+    <input type="text" name="pin" class="w3-input w3-border" required>
+    <input type="button" name="access" value="Access" class="w3-btn w3-teal w3-rounded">
+    
+
+
 if(isset($_POST['submit'])) {
   $id = sanitize($_GET['id']);
   $title = sanitize($_POST['title']);
@@ -17,6 +37,7 @@ $result = mysqli_query($dbcon, $sql);
   else {
     echo "Posted succesfully";
   }
+}
   
   mysqli_close($dbcon);
 
