aebc2d2ad449a6b8602b61c012b5d26f1f350508
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-27 14:07:16 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,4 +1,17 @@
-<?php
+<head>
+<title>PHP Blog</title>
+<meta name="viewport" content="width=device-width",initial-scale=1">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<div class="w3-container w3-teal">
+<h1>PHP Blog</h1>
+</div>
+<div class="w3-bar w3-border">
+<a href="index.php" class="w3-bar-item w3-button w3-pale-green">Home</a> 
+<a href="new.php" class="w3-bar-item w3-button">New Post</a>
+</div>
+<body>
 
 
-?>
+
