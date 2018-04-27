84c209d30e733ef1a37e19cc3cd315a4c54fad48
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-04-27 14:27:17 +0100

    Update newpost.php

--- newpost.php
+++ newpost.php
@@ -1,53 +1,50 @@
-<?php
-include('header.php');
-include('connect.php');
+<?php include("header.php");
+include("connect.php");
+?>
 
-// Protect page
-$user = "admin";
-$pin = "12345";
-if(isset($_POST['access'])) {
-  if(sanitize($user) == $user && sanitize($pin) == $pin) {
-    echo "<script>alert("Access Granted")</script>";
-  }
-  else {
-    die("Access Denied");
-  }
-  if(!isset($_POST['user']) && !isset($_POST['pin'])) {
-    echo "<form action="" method="POST" class="w3-panel w3-card-4">
-    <label>User</label>
-    <input type="text" name="user" class="w3-input w3-border" required>
-    <label>Pin</label>
-    <input type="text" name="pin" class="w3-input w3-border" required>
-    <input type="button" name="access" value="Access" class="w3-btn w3-teal w3-round">";
-    exit;
-  }
-}
-    echo "<div class="w3-container w3-teal"><h2>New Post</h2></div>";
-	echo "<form action="" method="POST" class="w3-container">
-	<label>Title</label>
-    <input type="text" name="title" class="w3-input w3-border" required>
-    <label>Description</label>
-    <input type="text" name="description" class="w3-input w3-border" required>
-    <input type="button" name="submit" value="Post" class="w3-btn w3-teal w3-round">";
-	
+<div class="w3-container w3-teal">
+<h2>New  Post</h2>
+</div>
+
+
+
+<form class="w3-container" action="<?php $_PHP_SELF ?>" method="POST">
+<label>Title</label>
+
+<input type="text" class="w3-input w3-border" name="title" required>
+<br>
+
+<label>Description</label>
+<textarea row="30" cols="50" class="w3-input w3-border" name="description" required/></textarea>
+<br>
+<input type="submit" class="w3-btn w3-teal w3-round" name="submit" value="Post">
+</form>
+
+
+
+<?php
+include("connect.php");
 
 if(isset($_POST['submit'])) {
-  $id = sanitize($_GET['id']);
-  $title = sanitize($_POST['title']);
-  $description = sanitize($_POST['description']);
-  $date = date("d M, Y");
 
-$sql = "INSERT INTO blog (id, title, description, date) VALUES($id, $title, $description, $date)";
+$id = $_POST['id'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+$date = date('Y-m-d H:i');
 
-$result = mysqli_query($dbcon, $sql);
-  if(!$result) {
-    echo "failed to post" . mysqli_connect_error();
-  }
-  else {
-    echo "Posted succesfully";
-  }
+
+$sql = "INSERT INTO blog (id, title, description, date) VALUES('$id', '$title', '$description', '$date')";
+mysqli_select_db($dbcon,'newblog');
+
+$query = mysqli_query($dbcon, $sql);
+if (!query) {
+die("Failed to post". mysqli_connect_error());
 }
-  
-  mysqli_close($dbcon);
+else {
+echo "<script>alert('Posted successfully')</script>";
+}
+}
 
+
+include("footer.php");
 ?>
