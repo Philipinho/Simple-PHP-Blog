cdb577988ecfe2eff6364a4805b9e4c37c908111
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-05-03 21:35:04 +0100

    Update

--- edit.php
+++ edit.php
@@ -1,3 +1,55 @@
 <?php
+include("header.php");
+include("connect.php");
 
+$id = (INT) $_GET['id'];
+if($id < 1) {
+	header("location: index.php");
+}
+
+$sql = "SELECT * FROM blog WHERE id = '$id'";
+$result = mysqli_query($dbcon, $sql);
+if(mysqli_num_rows($result) ==0) {
+	header("location: index.php");
+	} 
+$row = mysqli_fetch_assoc($result);
+$id = $row['id'];
+$title = $row['title'];
+$description = $row['description']; 
+
+if(isset($_POST['upd'])) {
+ $id = $_POST['id'];
+ $title = mysqli_real_escape_string($dbcon, $_POST['title']);
+ $description = mysqli_real_escape_string($dbcon, $_POST['description']);
+ 
+	$sql2 = "UPDATE blog SET title = '$title', description = '$description' WHERE id = $id";
+
+if(mysqli_query($dbcon, $sql2)) {
+// or die("failed to edit. ". mysqli_connect_error());
+	echo "Post edited successfully.";
+	echo "<meta http-equiv='refresh' content='2; url=view.php?id=$id' />";
+	
+	} 
+	else {
+		echo "failed to edit". mysqli_connect_error();
+		}
+		}
+?>
+
+<form action="" method="POST" class="w3-container">
+<input type="hidden" name="id" value="<?php echo $id;?>">
+<label>Title</label>
+<input type="text" class="w3-input w3-border" name="title" value="<?php echo $title;?>">
+
+<label>Description</label>
+<textarea class="w3-input w3-border" name="description"><?php echo $description;?> </textarea>
+
+<input type="submit" class="w3-btn w3-teal w3-round" name="upd" value="Edit">
+</form>
+
+
+<?php
+mysqli_close($dbcon);
+
+include("footer.php");
 ?>
