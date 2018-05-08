23432c9d447e75fcfc19496ee1daaf10c35aff33
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-05-08 16:28:45 +0100

    Update

--- newpost.php
+++ newpost.php
@@ -1,10 +1,4 @@
-<?php
-session_start();
-if(!isset($_SESSION['username'])) {
-	header("location: login.php");
-	exit();
-	} 
- include("header.php");
+<?php include("header.php");
 include("connect.php");
 $id = (INT) $_GET['id'];
 
@@ -16,9 +10,8 @@ $id = $_POST['id'];
 $title = mysqli_real_escape_string($dbcon, $_POST['title']);
 $description = mysqli_real_escape_string($dbcon, $_POST ['description']); 
 $date = date('Y-m-d H:i');
-$posted_by = mysqli_real_escape_string($dbcon, $_SESSION['username']);
 
-$sql = "INSERT INTO posts (id, title, description, posted_by, date) VALUES('$id', '$title', '$description', '$posted_by', '$date')";
+$sql = "INSERT INTO blog (id, title, description, date) VALUES('$id', '$title', '$description', '$date')";
 mysqli_query($dbcon, $sql) or die("failed to post". mysqli_connect_error());
 
   printf("Posted successfully. <meta http-equiv='refresh' content='2; url=view.php?id=%d'/>", mysqli_insert_id($dbcon));
@@ -29,15 +22,13 @@ else {
 	?>
 		
 
-<form class="w3-container" action="<?php htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="POST">
+<form class="w3-container" action="<?php $_PHP_SELF ?>" method="POST">
 <label>Title</label>
 
 <input type="text" class="w3-input w3-border" name="title" required>
-<br>
 
 <label>Description</label>
 <textarea id = "mytextareaj" row="30" cols="50" class="w3-input w3-border" name="description" required/></textarea>
-<br>
 
 <input type="submit" class="w3-btn w3-teal w3-round" name="submit" value="Post">
 </form>
