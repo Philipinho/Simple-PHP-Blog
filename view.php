fb34be604d1c0c1cadbea7266e62b3c2134263d7
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-05-08 16:29:29 +0100

    Update

--- view.php
+++ view.php
@@ -1,20 +1,18 @@
-
 <?php
-include("header.php");
+session_start();
+ include("header.php");
 include("connect.php");
-
 $id = (INT) $_GET['id'];
  if($id < 1) {
 header("location: index.php");
  } 
-
- $sql = "SELECT id, title, description, date FROM blog WHERE id = $id";
+$sql = "Select * FROM posts WHERE id = '$id'";
  
  $result = mysqli_query($dbcon, $sql);
  
   $invalid = mysqli_num_rows($result);
   if($invalid == 0) {
-  header("location: index.php");
+header("location: index.php");
   } 
  
  $row = mysqli_fetch_assoc($result);
@@ -22,29 +20,27 @@ header("location: index.php");
  $id = $row['id'];
  $title = $row['title'];
 $des = $row['description'];
-$time = $row['date'];
-
+$by = $row['posted_by']; 
+$time = $row['date'];  
+ 
 echo '<div class="w3-container w3-sand w3-card-4">';
 
 echo "<h2>$title</h2>";
 echo '<div class="w3-panel w3-pale green w3-card-4">';
 echo "$des<br>";
 echo '<div class="w3-text-grey">';
+echo $by."<br>";
 echo "$time</div>";
-
 ?>
 
 
-
+<?php
+if(isset($_SESSION['username'])) {
+	?>
  <div class="w3-text-green"><a href="edit.php?id=<?php echo $row['id'];?>">[Edit]</a></div>
 <div class="w3-text-red">
-<a href="delete.php?id=<?php echo $row['id'];?>" onclick="return confirm('Are you sure you want to delete this post?'); ">[Delete]</a></div> 
-
-
-
+<a href="del.php?id=<?php echo $row['id'];?>" onclick="return confirm('Are you sure you want to delete this post?'); ">[Delete]</a></div> 
 <?php
-
+}
 echo '</div></div>';
-
-
 include("footer.php"); ?> 
