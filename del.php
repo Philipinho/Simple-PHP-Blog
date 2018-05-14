bf2af68b1c8239497c22e4d594fd05958cbe9452
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-05-14 05:24:55 +0100

    Update

---
+++ del.php
@@ -0,0 +1,18 @@
+<?php
+include("connect.php");
+if(isset($_GET['id'])) {
+$id = $_GET['id'];
+ 
+$sql = "DELETE FROM posts WHERE id = '$id'";
+ 
+$result = mysqli_query($dbcon, $sql);
+if($result) {
+header('location: index.php');
+}
+else {
+	echo "Failed to delete.".mysqli_connect_error();
+}
+}
+mysqli_close($dbcon);
+?>
+ 
