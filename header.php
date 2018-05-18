84b3f2ee04676c394189d337e67b96d49baa5c45
Krypton <16838612+Kryptoniq@users.noreply.github.com>
2018-05-18 23:01:55 +0100

    W3.css

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ session_start();
 		<html>
 			<title>PHP Blog</title>
 			<meta name="viewport" content="width=device-width",initial-scale=1">
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="https://www.w3schools.com/w3css/4/w3.css">
 
 <script src='tinymce/tinymce.min.js'></script>
 
