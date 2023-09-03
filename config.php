c06a1f5619a940504a352273069ddc67a6a4d8dc
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-03 21:21:29 +0930

    Formatting.
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- config.php
+++ config.php
@@ -1,7 +1,4 @@
 <?php
-    define('SITE_ROOT', ''); // If installed on a sub-folder, replace the empty constant with the folder's name
-    define('PAGINATION', 10); // Pagination results per page
-
     /* Database credentials.*/
     define('DB_TYPE', 'mysql');         // NOT USED
     define('DB_SERVER', 'LOCALHOST');   // Server name/IP for the mysql server
@@ -9,3 +6,7 @@
     define('DB_PASSWORD', 'PASSWORD');  // What password to use when logging in
     define('DB_NAME', 'MySimpleBlog');  // What is the database we are using
     define('DB_CHARSET', 'utf8');       // What character set are we using
+
+    /* Define some settings for the blog */
+    define('SITE_ROOT', ''); // If installed on a sub-folder, replace the empty constant with the folder's name
+    define('PAGINATION', 10); // Pagination results per page
