0446201b18e18dcf42e447ede027504cbe70761c
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-03 20:50:48 +0930

    Updated style to use img 100%
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- view.php
+++ view.php
@@ -24,7 +24,7 @@ $description = $row['description'];
 $author = $row['posted_by'];
 $time = $row['date'];
 
-echo '<div class="w3-container w3-sand w3-card-4">';
+echo '<div class="w3-container w3-sand w3-card-4 img {width: 100%}">';
 
 echo "<h3>$title</h3>";
 echo '<div class="w3-panel w3-leftbar w3-rightbar w3-border w3-sand w3-card-4">';
