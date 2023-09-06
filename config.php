bbfad654dcfd10d9373020f4afd82ad77238bd7e
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-06 17:07:46 +0930

    Updated config to explain site_root better
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- config.php
+++ config.php
@@ -8,5 +8,5 @@
     define('DB_CHARSET', 'utf8');       // What character set are we using
 
     /* Define some settings for the blog */
-    define('SITE_ROOT', ''); // If installed on a sub-folder, replace the empty constant with the folder's name
+    define('SITE_ROOT', ''); // If installed on a sub-folder. E.g. if installed to 'www.example.com/blog', enter 'blog' for SITE_ROOT
     define('PAGINATION', 10); // Pagination results per page
