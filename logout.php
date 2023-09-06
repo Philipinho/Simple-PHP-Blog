850add31ce1395393e4b1a1d000d2a165b3e85cd
Nephiaust <29741794+Nephiaust@users.noreply.github.com>
2023-09-06 18:31:55 +0930

    Moved the redirect to index page, if logging out Moved the redirect to login page, if not logged in
    Signed-off-by: Nephiaust <29741794+Nephiaust@users.noreply.github.com>

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 if (!isset($_SESSION['username'])) {
-    header("location: index.php");
+    header("location: login.php");
 } else {
     session_destroy();
-    header("location: login.php");
+    header("location: index.php");
 }
